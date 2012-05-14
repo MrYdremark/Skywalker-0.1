95c0f1af37ea8d1235ddf7aeb625517eadb1b991
Anders <MrYdremark@gmail.com>
2012-05-14 11:34:42 +0200

    uppdelat

---
+++ nodes.rb
@@ -0,0 +1,313 @@
+class AdditionNode
+  def initialize(op, a, b)
+    @op, @a, @b = op, a, b
+  end
+
+  def evaluate
+    eval("#{@a.evaluate}#{@op}#{@b.evaluate}")
+  end
+
+  def debug
+    " -> AdditionNode(#{@a.debug} #{@op} #{@b.debug})"
+  end
+
+  def compile
+    "#{@a.compile} #{@op} #{@b.compile}"
+  end
+end
+
+class MultiNode
+  def initialize(op, a, b)
+    @op, @a, @b = op, a, b
+  end
+
+  def evaluate
+    eval("#{@a.evaluate}#{@op}#{@b.evaluate}")
+  end
+
+  def debug
+    " -> MultiNode(#{@a.debug} #{@op} #{@b.debug})"
+  end
+
+  def compile
+    "#{@a.compile} #{@op} #{@b.compile}"
+  end
+end
+
+class IntegerNode
+  def initialize(a)
+    @a = a
+  end
+
+  def evaluate
+    @a
+  end
+
+  def debug
+    " -> IntegerNode(#{@a})"
+  end
+
+  def compile
+    "#{@a}"
+  end
+end
+
+class FloatNode
+  def initialize(a)
+    @a = a
+  end
+
+  def evaluate
+    @a
+  end
+
+  def debug
+    " -> FloatNode(#{@a})"
+  end
+
+  def compile
+    "#{@a}"
+  end
+end
+
+class AssignNode
+  def initialize(a,b)
+    @a = a
+    @b = b
+  end
+
+  def evaluate
+    c = @b.evaluate
+    assignVariable(@a,c)
+  end
+
+  def debug
+    c = @b.evaluate
+    " -> AssignNode(#{assignVariable(@a,c)})"
+  end
+
+  def assignVariable(a,b)
+    @@vars[a] = b
+  end
+
+  def compile
+    "#{@a} = #{@b.compile}"
+  end
+end
+
+class ControlsAssignNode
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+
+  def compile
+    "@@control[\"#{@a}\"] = #{@b.compile}"
+  end
+end
+
+class IncludeNode
+  def initialize(a)
+    @@external = a
+  end
+
+  def compile
+    ""
+  end
+end
+
+class InterfaceVarNode
+  def initialize(a)
+    @a = a
+  end
+
+  def compile
+    "#{@a}"
+  end
+end
+
+class InterfaceVarListNode < Array
+  def initialize
+  end
+
+  def compile
+    self.each {|a| @@control[a] = 0 }
+  end
+end
+              
+
+class IfNode
+  def initialize(a, b)
+    @bool = a
+    @stmt = b
+  end
+
+  def evaluate
+    if @bool.evaluate
+      @stmt.evaluate
+    else
+      nil
+    end
+  end
+
+  def debug
+    " -> If -> Bool(#{@bool.debug}) then -> #{@stmt.debug}"
+    nil
+  end
+
+  def compile
+    "if (#{@bool.compile})\n#{@stmt.compile}"
+  end
+end
+
+class ElseNode
+  def initialize(a)
+    @stmt = a
+  end
+
+  def compile
+    "else\n#{@stmt.compile}"
+  end
+end
+
+class IfElseNode
+  def initialize(a,b)
+    @if = a
+    @else = b
+  end
+
+  def compile
+    "#{@if.compile}\n#{@else.compile}\nend"
+  end
+end
+
+class WhileNode
+  def initialize(a, b)
+    @bool = a
+    @stmt = b
+  end
+
+  def evaluate
+    while(true)
+      if @bool.evaluate
+        @stmt.evaluate
+      else
+        break
+      end
+    end
+  end
+
+  def debug
+    " -> While -> Bool(#{@bool.debug} do -> #{@stmt.debug})"
+  end
+
+  def compile
+    "while(#{@bool.compile})\n#{@stmt.compile}\nend"
+  end
+end
+
+class WaitNode
+  def initialize(a)
+    @a = a
+  end
+
+  def evaluate
+    sleep @a.evaluate
+  end
+
+  def debug
+    " -> wait #{@a.debug} sek"
+  end
+
+  def compile
+    "skywalker_update\nsleep(#{@a.compile})"
+  end
+end
+
+class IdentifierNode
+  def initialize(a)
+    @a = a
+  end
+
+  def evaluate
+    @@vars[@a]
+  end
+  
+  def debug
+    " -> IdentifierNode(#{@a}) = (#{@@vars[@a]})"
+  end
+
+  def compile
+    "#{@a}"
+  end
+end
+
+class BooleanNode
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def evaluate
+    eval("#{@a.evaluate}#{@b}#{@c.evaluate}")
+  end
+
+  def debug
+    " -> Bool(#{@a.evaluate}#{@b}#{@c.evaluate})"
+  end
+
+  def compile
+    "#{@a.compile}#{@b}#{@c.compile}"
+  end
+end
+
+class Dummy
+  def initialize
+    nil
+  end
+  def evaluate
+    ""
+  end
+  def compile
+    ""
+  end
+end
+
+class StmtNode
+  def initialize(a)
+    @a = a
+  end
+  
+  def evaluate
+    @a.evaluate
+  end
+  
+  def debug
+    " -> StmtNode(#{@a.debug})"
+  end
+
+  def compile
+    "#{@a.compile}\n"
+  end
+end
+
+class StmtListNode < Array
+  def initialize
+  end
+  def evaluate
+    self.each {|a| a.evaluate }
+    self[-1].evaluate
+  end
+
+  def debug
+    self.each {|a| puts "StmtListNode #{a.debug} \n"}
+  end
+
+  def compile
+    derp = ""
+    self.each {|a| derp += a.compile}
+
+    derp
+  end
+end
---
+++ skywalker.rb
@@ -0,0 +1,212 @@
+require 'parser.rb'
+require 'nodes.rb'
+
+class Skywalker
+  def initialize
+    @skywalker = Parser.new("skywalker") do
+      token(/\s+/)
+      token(/\d+\.\d+/) {|m| m.to_f }
+      token(/\d+/) {|m| m.to_i }
+      token(/\w+/) {|m| m }
+      token(/./) {|m| m }
+      
+      start :start do
+        match(:interface, :routine) {|a, b| @@interface = a, @@code = b }
+      end
+      
+      rule :interface do
+        match("interface", :interface_stmt_list, :terminator) {|a| a }
+      end
+      
+      rule :routine do
+        match("routine", "Main", :stmt_list, :terminator) {|_, _, a, _| a }
+      end
+
+      rule :stmt_list do
+        match(:stmt) {|a| StmtListNode.new << StmtNode.new(a)}
+        match(:stmt_list, ";", :stmt) {|a, _, b| a << StmtNode.new(b)}
+      end
+
+      rule :interface_stmt_list do
+        match(:interface_stmt) {|a| StmtListNode.new << StmtNode.new(a)}
+        match(:interface_stmt_list, ";", :interface_stmt) {|a, _, b|
+          a << StmtNode.new(b)}
+      end
+
+      rule :interface_stmt do
+        match(:include_stmt) {|a| a }
+        match(:interface_assign_stmt) {|a| a }
+      end
+
+      rule :include_stmt do
+        match("external", "(", /.+/, ".", /.+/, ")") {|_, _, a, b, c, _|
+          IncludeNode.new("#{a}#{b}#{c}") }
+      end
+
+      rule :interface_assign_stmt do
+        match("Controls", "=", "{", :interface_var_list, "}") {|_, _, _, a, _|
+          ControlsAssignNode.new(a, 0) } 
+      end
+
+      rule :interface_var_list do
+        match(:interface_var) {|a|
+          InterfaceVarListNode.new << InterfaceVarNode.new(a) }
+        match(:interface_var_list, ",", :interface_var) {|a, _, b|
+          a << InterfaceVarNode.new(b) }
+      end
+
+      rule :interface_var do
+        match(/[a-zA-Z]/) {|a| a }
+      end
+
+      rule :control_assign_stmt do
+        match("Controls", "[", /[a-zA-Z]/, "]", "=", :addition) {|_, _, a, _, _, b|
+          ControlsAssignNode.new(a, b) } 
+      end
+      
+      rule :stmt do
+        match(:if_else_stmt) {|a| a}
+        match(:while_stmt) {|a| a}
+        match(:control_assign_stmt) {|a| a}
+        match(:assign_stmt) {|a| a}
+        match(:wait_stmt) {|a| a}
+        match(:addition) {|a| a}
+      end
+
+      rule :addition do
+        match(:multi)
+        match(:addition, :addition_oper, :multi) {|a, c, b|
+          AdditionNode.new(c, a, b) }
+      end
+      
+      rule :multi do
+        match(:primary)
+        match(:multi, :multi_oper, :multi) {|a, c, b| MultiNode.new(c, a, b) }
+      end
+      
+      rule :primary do
+        match("(", :addition, ")") {|_, a, _| a }
+        match(:atom)
+      end
+      
+      rule :atom do
+        match(Float) {|a| FloatNode.new(a) }
+        match(Integer) {|a| IntegerNode.new(a) }
+        match(:identifier)        
+        match(";")
+      end
+
+      rule :boolean do
+        match("true") {|a| a}
+        match("false") {|a| a}
+      end
+      
+      rule :terminator do
+        match("end") {|a| a}
+      end
+      
+      rule :addition_oper do
+        match("+") {|a| a}
+        match("-") {|a| a}
+      end
+
+      rule :multi_oper do
+        match("*") {|a| a}
+        match("/") {|a| a}
+      end
+
+      rule :rel_oper do
+        match("==")
+        match("!=")
+        match("<=")
+        match(">=")
+        match("<")
+        match(">")
+      end
+
+      rule :bool_expr do
+        match(:boolean) {|a| BooleanNode.new(Dummy.new, a, Dummy.new)}
+        match(:addition, :rel_oper, :multi) {|a, b, c| BooleanNode.new(a, b, c) }
+      end
+      
+      
+      rule :if_stmt do
+        match("if", "(", :bool_expr, ")", :stmt_list) {|_, _, a, _, b|
+          IfNode.new(a,b) }
+      end
+
+      rule :else_stmt do
+        match("else", :stmt_list) {|_, a| ElseNode.new(a) }
+      end
+
+      rule :if_else_stmt do
+        match(:if_stmt, :terminator) {|a, _| IfElseNode.new(a,"") }
+        match(:if_stmt, :else_stmt, :terminator) {|a, b, _|
+          IfElseNode.new(a,b) }
+      end
+
+      rule :while_stmt do
+        match("while", "(", :bool_expr, ")", :stmt_list, :terminator) {
+          |_, _, a, _, b, _|
+          WhileNode.new(a,b) }
+      end
+
+      rule :wait_stmt do
+        match("wait", "(", :addition, ")") {|_, _, a, _| WaitNode.new(a)}
+      end
+      
+      rule :identifier do
+        match(/^[a-z]+/) {|a| IdentifierNode.new(a) }
+      end
+
+      rule :assign_stmt do
+        match(/^[a-z]+/, "=", :addition) {|a, _, b| AssignNode.new(a,b) }
+      end
+      
+    end
+  end
+
+  def done(str)
+    ["quit","exit","bye",""].include?(str.chomp)
+  end
+  
+  def run
+    print "[skywalker] "
+    str = gets
+    if done(str) then
+      puts "Bye."
+    else
+      @@res = (@skywalker.parse str)
+      puts "=> #{@@res.evaluate}"
+      # @skywalker.parse(str)
+      run
+    end
+  end
+
+  def runfile(filename)
+    code = File.read(filename)
+    @@res = (@skywalker.parse code)
+    puts "=> #{@@res.evaluate}"
+  end
+
+  def compile(filename)
+    code = File.read(filename)
+    @@res = (@skywalker.parse code)
+    external = File.read(@@external)
+    File.open("out.rb", 'w') {|f| f.write(external +
+                                          "\n" +
+                                          "def main\n" +
+                                          @@code.compile +
+                                          "\nskywalker_update" +
+                                          "\nskywalker_end" +
+                                          "\nend\nmain") }
+  end
+  
+  def log(state = false)
+    if state
+      @skywalker.logger.level = Logger::DEBUG
+    else
+      @skywalker.logger.level = Logger::WARN
+    end
+  end
+end
